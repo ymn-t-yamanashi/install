0b7bd0596b4114e1864784db4d2785e77be19adf
t-yamanashi <ae86bg5@yahoo.co.jp>
2021-09-22 10:28:38 +0900

    Add docker-compose

--- docker.sh
+++ docker.sh
@@ -10,3 +10,10 @@ cat  << EOS >> ~/.bashrc
 export PATH=/usr/bin:\$PATH
 export DOCKER_HOST=unix:///run/user/1000/docker.sock
 EOS
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
