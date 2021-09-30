c4231f2db8306bc225508ee26f0708880f3cd57d
Tomoyuki Yamanashi <ae86bg5@yahoo.co.jp>
2021-09-30 12:24:27 +0900

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -11,11 +11,6 @@ export PATH=/usr/bin:\$PATH
 export DOCKER_HOST=unix:///run/user/1000/docker.sock
 EOS
 
-#docker-compose old
-#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose
-#sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
 curl -L https://raw.githubusercontent.com/docker/compose-cli/main/scripts/install/install_linux.sh | sh
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.0.0/docker-compose-linux-amd64 -o ~/.docker/cli-plugins/docker-compose
