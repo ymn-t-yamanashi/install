f450abe04615e8fc61d0066c7ced038140611a7b
t-yamanashi <ae86bg5@yahoo.co.jp>
2022-11-11 10:27:17 +0900

    add vscode  install extension

---
+++ vscode_ext.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#Elixir
+code --install-extension jakebecker.elixir-ls
+code --install-extension ritvyk.heex-html
+
+#docker
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-azuretools.vscode-docker
+
+#editorconfig
+code --install-extension editorconfig.editorconfig
+
+#etc
+code --install-extension mhutchie.git-graph
+code --install-extension alefragnani.bookmarks
+code --install-extension ms-vsliveshare.vsliveshare
