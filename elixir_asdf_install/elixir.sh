3ac7081a3848890534f927b292d68831f9fb170d
Tomoyuki YaMaNashi <ae86bg5@yahoo.co.jp>
2022-12-16 22:18:13 +0900

    Update elixir.sh

--- elixir_asdf_install/elixir.sh
+++ elixir_asdf_install/elixir.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-sudo apt -y --fix-broken install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop
-
+sudo apt -y --fix-broken install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libwxgtk-webview3.0-gtk3-dev
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 
-asdf install erlang 24.0
-asdf install elixir 1.12.2
-asdf global erlang 24.0
-asdf global elixir 1.12.2
+asdf install erlang 25.0.4
+asdf install elixir 1.13.4-otp-25
+asdf global erlang 25.0.4
+asdf global elixir 1.13.4-otp-25
