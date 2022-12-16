b002f8eb32a56ba5d420a1d25674ff5f04b06f03
Tomoyuki YaMaNashi <ae86bg5@yahoo.co.jp>
2022-12-16 22:03:39 +0900

    Update asdf.sh

--- elixir_asdf_install/asdf.sh
+++ elixir_asdf_install/asdf.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 sudo apt update
-sudo apt install git curl git
+sudo apt install git curl git -y
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
 echo ". $HOME/.asdf/asdf.sh" >> ~/.bashrc
 echo ". $HOME/.asdf/completions/asdf.bash" >> ~/.bashrc
-source ~/.bashrc
-
+echo "type source ~/.bashrc"
