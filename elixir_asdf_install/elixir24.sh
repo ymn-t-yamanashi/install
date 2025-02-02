b74dd301d981ff6c9e6faec4112eac61b97fd48f
t-yamanashi <ae86bg5@yahoo.co.jp>
2025-02-02 16:18:34 +0900

    libncurses5-devは不要なため削除

--- elixir_asdf_install/elixir24.sh
+++ elixir_asdf_install/elixir24.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt install -y build-essential autoconf m4 libncurses5-dev libwxgtk3.2-dev libwxgtk-webview3.2-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
+sudo apt install -y build-essential autoconf m4 libwxgtk3.2-dev libwxgtk-webview3.2-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 
